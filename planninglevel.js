96398474b1720006b4d669a2092d424d922d7102
Kevin <kdilts13@gmail.com>
2014-03-03 22:49:00 -0700

    Continuing to work on planninglevel.js

--- planninglevel.js
+++ planninglevel.js
@@ -1,6 +1,7 @@
 Application.PlanningLevel = function(){
 	this.width;
 	this.height;
+	this.numColors = 4;
 	this.grid = []; // grid[x][y][color] = instruction
 	this.undoStack = [];
 	this.redoStack = [];
@@ -8,9 +9,11 @@ Application.PlanningLevel = function(){
 
 	this.getCell = function(x,y){ return this.grid[x][y]; };
 
+	// clears an entire cell
 	this.removeCell = function(x,y){
-		for(var c = 0; c < this.numColors; )
-		if(this.grid[x][y][c]){ this.grid[x][y][c] = null; };
+		for(var c = 0; c < this.numColors; c++){
+			if(this.grid[x][y][c]){ this.grid[x][y][c] = null; };
+		}
 	};
 	
 	this.forEachCell = function(f){
@@ -25,6 +28,9 @@ Application.PlanningLevel = function(){
 
 	// moves an instruction from one cell to another, does the reverse if r is set to 1
 	this.moveOp = function(x,y,c,nx,ny,r){
+
+		this.x = x; this.y = y; this.c = c;
+		this.nx = nx; this.ny = ny; this.r = r;
 
 		this.apply = function(){
 			if(this.r !== 1){
@@ -42,6 +48,9 @@ Application.PlanningLevel = function(){
 	// inserts an instruction, does the reverse if r is set to 1
 	this.insertOp = function(x,y,ins,r){
 		
+		this.x = x; this.y = y;
+		this.ins = ins; this.r = r;
+
 		this.apply = function(){
 			if(r !== 1){
 				this.grid[x][y][c] = ins;
@@ -55,6 +64,9 @@ Application.PlanningLevel = function(){
 	// deletes an instruction, does the reverse if r is set to 1
 	this.deleteOp = function(x,y,c,r){
 		
+		this.x = x; this.y = y;
+		this.c = c; this.r = r;
+
 		this.apply = function(){
 			if(r !== 1){
 				this.deletions.push(this.grid[x][y][c]);
