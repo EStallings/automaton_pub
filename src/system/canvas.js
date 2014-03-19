502755feee67fb37f5accc210a707717d72c1903
Ezra <estallings92@gmail.com>
2014-03-19 16:03:49 -0600

    fixed static redraw for gui with window resize

--- src/system/canvas.js
+++ src/system/canvas.js
@@ -34,6 +34,7 @@ App.createCanvasArray = function(){
 
 		// TODO: everything needs to be re-rendered
 		if(App.Game)App.Game.requestStaticRenderUpdate = true;
+		if(App.Gui)App.Gui.drawStatic = true;
 	}
 
 	window.onresize(); // initializes width and height on page-load
