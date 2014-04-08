e3765d9bddf50a7a75b1ad7832e2acc6ee731411
Kevin <kdilts13@gmail.com>
2014-04-07 18:18:25 -0600

    Created a debug display for myself.

--- src/game/planning/planningGraphics.js
+++ src/game/planning/planningGraphics.js
@@ -5,7 +5,21 @@ App.PlanningGraphics = function(){
 	this.staticRender = function(gfx){
 		if(App.Game.currentPlanningLevel.currentSelection.length !== 0
 				&& App.Game.currentPlanningLevel.currentSelection[0] !== null ){ that.selectionOverlay(gfx); }
+	}
+
+	this.debug = function(gfx){
+		gfx.fillStyle = '#ffffff';
+		gfx.font='20px Georgia';
 		
+		gfx.fillText('mouse x: ' + App.Game.currentPlanningLevel.input.downX +
+			'     mouse y: ' + App.Game.currentPlanningLevel.input.downY +
+			'     mouse c: ' + App.Game.currentPlanningLevel.input.downC,
+			 100,100);
+		
+		var instruction = App.Game.currentPlanningLevel.currentSelection[0];
+		gfx.fillText('instruction x: ' + instruction.x + '     instruction y: ' + instruction.y +
+			'     instruction c: ' + instruction.color, 100,122);
+
 	}
 
 	this.selectionOverlay = function(gfx){
@@ -17,6 +31,8 @@ App.PlanningGraphics = function(){
 
 	this.dynamicRender = function(gfx){
 		App.Game.tempGfx.clearRect(0,0,App.Canvases.width,App.Canvases.height);
+		//if(App.Game.currentPlanningLevel.currentSelection[0] !== null &&
+				//App.Game.currentPlanningLevel.currentSelection.length !== 0){ that.debug(gfx); }
 		if(App.Game.currentPlanningLevel.input.isDown){ that.drawSelectionBox(gfx); }
 	}
 
