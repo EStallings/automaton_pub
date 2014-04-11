7229061d968c7b8d5c330d62268621c31418786c
xdarkzero <Traxypad@gmail.com>
2014-04-11 11:33:57 -0600

    added missing files

---
+++ src/game/planGui.js
@@ -0,0 +1,102 @@
+App.setupPlanGui = function(){
+	var planMode = App.ModeHandler.addNewMode('planning');
+
+		// ---------------------------------------------
+
+	planMode.gfx = App.Canvases.addNewLayer('planning gui',1);
+	planMode.direction = App.DIRECTIONS.UP;
+
+		// ---------------------------------------------
+
+	planMode.enterFunc = function(){
+		planMode.requestStaticRenderUpdate = true;
+		planMode.updatingActive = true;
+		planMode.exitFlag = false;
+	}
+
+	planMode.updateFunc = function(){
+		if(!planMode.requestStaticRenderUpdate)return;
+		planMode.requestStaticRenderUpdate = false;
+
+		planMode.gfx.clearRect(0,0,App.Canvases.width,App.Canvases.height);
+
+		var x=0;
+		App.InstCatalog.render(planMode.gfx, 0+planMode.direction,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx, 4,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx,12,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx,15,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx,13,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx,18+planMode.direction,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx,26+planMode.direction,10+x++*50,10,0,48);
+		App.InstCatalog.render(planMode.gfx, 8,10+x++*50,10,0,48);
+
+		var x=0;
+		App.InstCatalog.render(planMode.gfx, 7,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx, 6,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx, 5,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx,16,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx,14,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx,22+planMode.direction,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx,17,10+x++*50,10+50,0,48);
+		App.InstCatalog.render(planMode.gfx, 9,10+x++*50,10+50,0,48);
+
+		if(planMode.exitFlag && planMode.requestStaticRenderUpdate === false){
+			planMode.gfx.clearRect(0,0,App.Canvases.width,App.Canvases.height);
+			planMode.updatingActive = false;
+		}
+	}
+
+	planMode.exitFunc = function(){
+		planMode.requestStaticRenderUpdate = true;
+		planMode.exitFlag = true;
+	}
+
+		// ---------------------------------------------
+
+	planMode.registerKeyDownFunc('Esc',function(){
+		App.ModeHandler.popMode();
+		App.GameRenderer.bestFit();
+	});
+
+	planMode.registerKeyDownFunc('Space',function(){
+		App.ModeHandler.pushMode('simulation');
+		App.Game.toggleMode();
+	});
+
+	planMode.registerKeyDownFunc('W',function(){
+		planMode.direction = App.DIRECTIONS.UP;
+		planMode.requestStaticRenderUpdate = true;
+	});
+
+	planMode.registerKeyDownFunc('S',function(){
+		planMode.direction = App.DIRECTIONS.DOWN;
+		planMode.requestStaticRenderUpdate = true;
+	});
+
+	planMode.registerKeyDownFunc('A',function(){
+		planMode.direction = App.DIRECTIONS.LEFT;
+		planMode.requestStaticRenderUpdate = true;
+	});
+
+	planMode.registerKeyDownFunc('D',function(){
+		planMode.direction = App.DIRECTIONS.RIGHT;
+		planMode.requestStaticRenderUpdate = true;
+	});
+
+	planMode.registerMouseMoveFunc(function(x,y){
+		App.GameRenderer.screenToGridCoords(x,y);
+		if(App.InputHandler.rmb)App.GameRenderer.pan(x,y);
+	});
+
+	planMode.registerMouseDownFunc(App.InputHandler.MOUSEBUTTON.RIGHT,function(x,y){
+		App.GameRenderer.beginPan(x,y);
+	});
+
+	planMode.registerMouseWheelFunc(function(w){
+		App.GameRenderer.zoom(App.InputHandler.mouseX,App.InputHandler.mouseY,w);
+	});
+
+	planMode.registerResizeFunc(function(){
+		// TODO: move grid relative to center of screen, NOT a bestFit()
+	});
+}
---
+++ src/game/simGui.js
@@ -0,0 +1,56 @@
+App.setupSimGui = function(){
+	var simMode = App.ModeHandler.addNewMode('simulation');
+
+		// ---------------------------------------------
+
+	simMode.gfx = App.Canvases.addNewLayer('simulation gui',1);
+
+		// ---------------------------------------------
+
+	simMode.enterFunc = function(){
+		simMode.requestStaticRenderUpdate = true;
+		simMode.updatingActive = true;
+		simMode.exitFlag = false;
+	}
+
+	simMode.updateFunc = function(){
+		if(!simMode.requestStaticRenderUpdate)return;
+		simMode.requestStaticRenderUpdate = false;
+
+		simMode.gfx.clearRect(0,0,App.Canvases.width,App.Canvases.height);
+
+		if(simMode.exitFlag && simMode.requestStaticRenderUpdate === false){
+			simMode.gfx.clearRect(0,0,App.Canvases.width,App.Canvases.height);
+			simMode.updatingActive = false;
+		}
+	}
+
+	simMode.exitFunc = function(){
+		simMode.requestStaticRenderUpdate = true;
+		simMode.exitFlag = true;
+	}
+
+		// ---------------------------------------------
+
+	simMode.registerKeyDownFunc('Space',function(){
+		App.ModeHandler.popMode();
+		App.Game.toggleMode();
+	});
+
+	simMode.registerMouseMoveFunc(function(x,y){
+		App.GameRenderer.screenToGridCoords(x,y);
+		if(App.InputHandler.rmb)App.GameRenderer.pan(x,y);
+	});
+
+	simMode.registerMouseDownFunc(App.InputHandler.MOUSEBUTTON.RIGHT,function(x,y){
+		App.GameRenderer.beginPan(x,y);
+	});
+
+	simMode.registerMouseWheelFunc(function(w){
+		App.GameRenderer.zoom(App.InputHandler.mouseX,App.InputHandler.mouseY,w);
+	});
+
+	simMode.registerResizeFunc(function(){
+		// TODO: move grid relative to center of screen, NOT a bestFit()
+	});
+}
