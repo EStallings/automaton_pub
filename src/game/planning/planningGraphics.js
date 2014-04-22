b8ff5e26135cf83d1fe3efa5b38edc8c79c2bfb3
Kevin <kdilts13@gmail.com>
2014-04-21 19:44:33 -0600

    Graphics for drag select, selection overlay, and move / copy are back.

--- src/game/planning/planningGraphics.js
+++ src/game/planning/planningGraphics.js
@@ -1,9 +1,80 @@
 App.PlanningGraphics = function(){
 
-	// TODO: graphics for move
-	// TODO: graphics for copy
-
 	var that = this;
+
+	this.lmb = ['up',-1,-1,-1,-1,-1]; // [button state, scrnX, scrnY, cell x, cell y, cell c]
+	this.mmb = ['up',-1,-1,-1,-1,-1];
+	this.rmb = ['up',-1,-1,-1,-1,-1];
+	this.mousePos = [-1,-1,-1,-1,-1]; // current mouse position [scrnX, scrnY, cellX, cellY, cellC]
+
+	this.moving = false;
+
+	this.mouseMove = function(cellX, cellY){
+		that.mousePos[0] = App.InputHandler.mouseX;
+		that.mousePos[1] = App.InputHandler.mouseY;
+		that.mousePos[2] = cellX;
+		that.mousePos[3] = cellY;
+		that.mousePos[4] = App.GameRenderer.mouseC;
+
+		if( (that.mousePos[2] !== that.mmb[3] || that.mousePos[3] !== that.mmb[4]) && that.lmb[0] === 'down'){
+			that.moving = true;
+		}
+		else{ that.moving = false; }
+	}
+
+	this.mouseDown = function(button, cellX, cellY){
+		if(button === 'lmb'){
+			that.lmb[0] = 'down';
+			that.lmb[1] = App.InputHandler.mouseX;
+			that.lmb[2] = App.InputHandler.mouseY;
+			that.lmb[3] = cellX;
+			that.lmb[4] = cellY;
+			that.lmb[5] = App.GameRenderer.mouseC;
+		}
+		else if(button === 'mmb'){
+			that.mmb[0] = 'down';
+			that.mmb[1] = App.InputHandler.mouseX;
+			that.mmb[2] = App.InputHandler.mouseY;
+			that.mmb[3] = cellX;
+			that.mmb[4] = cellY;
+			that.mmb[5] = App.GameRenderer.mouseC;
+		}
+		else{
+			that.rmb[0] = 'down';
+			that.rmb[1] = App.InputHandler.mouseX;
+			that.rmb[2] = App.InputHandler.mouseY;
+			that.rmb[3] = cellX;
+			that.rmb[4] = cellY;
+			that.rmb[5] = App.GameRenderer.mouseC;
+		}
+	}
+
+	this.mouseUp = function(button, cellX, cellY){
+		if(button === 'lmb'){
+			that.lmb[0] = 'up';
+			that.lmb[1] = App.InputHandler.mouseX;
+			that.lmb[2] = App.InputHandler.mouseY;
+			that.lmb[3] = cellX;
+			that.lmb[4] = cellY;
+			that.lmb[5] = App.GameRenderer.mouseC;
+		}
+		else if(button === 'mmb'){
+			that.mmb[0] = 'up';
+			that.mmb[1] = App.InputHandler.mouseX;
+			that.mmb[2] = App.InputHandler.mouseY;
+			that.mmb[3] = cellX;
+			that.mmb[4] = cellY;
+			that.mmb[5] = App.GameRenderer.mouseC;
+		}
+		else{
+			that.rmb[0] = 'up';
+			that.rmb[1] = App.InputHandler.mouseX;
+			that.rmb[2] = App.InputHandler.mouseY;
+			that.rmb[3] = cellX;
+			that.rmb[4] = cellY;
+			that.rmb[5] = App.GameRenderer.mouseC;
+		}
+	}
 
 	this.staticRender = function(gfx){}
 
@@ -62,36 +133,32 @@ App.PlanningGraphics = function(){
 			&& App.Game.currentPlanningLevel.currentSelection[0] !== null ){ that.selectionOverlay(gfx); } // TODO: move to static render?
 
 		// drag selection box
-		//if(App.InputHandler.mmb === true){ that.drawSelectionBox(gfx); }
+		if(App.InputHandler.mmb === true){ that.drawSelectionBox(gfx); }
 
 		// move / copy graphics
-		//if(App.Game.currentPlanningLevel.currentSelection.length !== 0
-		//	&& App.Game.currentPlanningLevel.input.moveStart[0] !== -1){ that.moveCopy(gfx); }
+		if(App.Game.currentPlanningLevel.currentSelection.length !== 0
+			&& that.moving){ that.moveCopy(gfx); }
 	}
 
-	/*this.moveCopy = function(gfx){
-		console.log('move / copy');
-		var mX = App.Game.currentPlanningLevel.input.scrnX;
-		var mY = App.Game.currentPlanningLevel.input.scrnY;
+	this.moveCopy = function(gfx){
+		var mX = that.mousePos[0];
+		var mY = that.mousePos[1];
 		gfx.strokeStyle = 'rgba(200,200,200,.5)';
 		gfx.beginPath();
-		gfx.moveTo(App.Game.currentPlanningLevel.input.moveStart[3], App.Game.currentPlanningLevel.input.moveStart[4]);
+		gfx.moveTo(that.lmb[1], that.lmb[2]);
 		gfx.lineTo(mX, mY);
 		gfx.stroke();
-	}*/
+	}
 
-	/*this.drawSelectionBox = function(gfx){
-		console.log('box select');
-		var mouseData = App.Game.currentPlanningLevel.input;
-		var curX = mouseData.scrnX;
-		var curY = mouseData.scrnY;
-		var downX = App.Game.currentPlanningLevel.input.mmb[1];
-		var downY = App.Game.currentPlanningLevel.input.mmb[2];
+	this.drawSelectionBox = function(gfx){	
+		var curX = that.mousePos[0];
+		var curY = that.mousePos[1];
+		var downX = that.mmb[1];
+		var downY = that.mmb[2];
 		gfx.fillStyle = 'rgba(255,255,255,0.1)';
 		gfx.fillRect(curX, curY, (downX-curX), (downY-curY) );
 		gfx.strokeStyle = '#ffffff';
 		gfx.lineWidth = 2;
 		gfx.strokeRect(curX, curY, (downX-curX), (downY-curY) );
-	//	gfx.strokeRect(curX-1, curY-1, (downX-curX)-1, (downY-curY)-1 );
-	}*/
+	}
 }
