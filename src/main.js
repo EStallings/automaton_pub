c2c4dd6f23f88fb438faba2ce7278a4b0833bce1
xdarkzero <lbalaor@cs.unm.edu>
2014-04-27 14:16:50 -0600

    removed coming soon mode

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ window.onload = function(){
 	App.Game         = App.makeGame();
 	App.GameRenderer = App.makeGameRenderer();
 
-	App.setupComingSoon();
 	App.setupMainMenu();
 	App.setupLevelSelect();
 	App.setupLibrary();
