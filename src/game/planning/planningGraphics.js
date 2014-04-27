2b74d9665ae9a4c7cdc999f902552990648d3f53
Kevin <kdilts13@gmail.com>
2014-04-26 19:24:10 -0600

    In the process of changing mouse stuff. Graphics work with the new control scheme, as does selection. Move / copy are next.

--- src/game/planning/planningGraphics.js
+++ src/game/planning/planningGraphics.js
@@ -7,6 +7,10 @@ App.PlanningGraphics = function(){
 	this.rmb = ['up',-1,-1,-1,-1,-1];
 	this.mousePos = [-1,-1,-1,-1,-1]; // current mouse position [scrnX, scrnY, cellX, cellY, cellC]
 
+	this.lmbDown = []; this.lmbUp = [];
+	this.lmbDrag = false; this.lmbStartOnTile = false;
+
+	this.inserted = false;
 	this.moving = false;
 	this.copying = false;
 
@@ -31,6 +35,18 @@ App.PlanningGraphics = function(){
 			that.lmb[3] = cellX;
 			that.lmb[4] = cellY;
 			that.lmb[5] = App.GameRenderer.mouseC;
+
+			that.lmbDown[0] = cellX;
+			that.lmbDown[1] = cellY;
+			that.lmbDown[2] = App.GameRenderer.mouseC;
+
+			var instr = App.Game.currentPlanningLevel.getInstruction(cellX, cellY, App.GameRenderer.mouseC);
+			if(instr && App.Game.currentPlanningLevel.currentSelection.indexOf(instr) !== -1){
+				that.lmbStartOnTile = true;
+			}
+			else{
+				that.lmbStartOnTile = false;
+			}
 		}
 		else if(button === 'mmb'){
 			that.mmb[0] = 'down';
@@ -58,6 +74,15 @@ App.PlanningGraphics = function(){
 			that.lmb[3] = cellX;
 			that.lmb[4] = cellY;
 			that.lmb[5] = App.GameRenderer.mouseC;
+
+			that.lmbUp[0] = cellX;
+			that.lmbUp[1] = cellY;
+			that.lmbUp[2] = App.GameRenderer.mouseC;
+
+			if(that.lmbDown[0] === that.lmbUp[0] && that.lmbDown[1] === that.lmbUp[1]
+				&& that.lmbDown[2] === that.lmbUp[2]){ that.lmbDrag = false; } else { that.lmbDrag = true; }
+
+			if(that.lmbDrag){ that.drag(); } else { that.single(); }
 		}
 		else if(button === 'mmb'){
 			that.mmb[0] = 'up';
@@ -74,6 +99,24 @@ App.PlanningGraphics = function(){
 			that.rmb[3] = cellX;
 			that.rmb[4] = cellY;
 			that.rmb[5] = App.GameRenderer.mouseC;
+		}
+	}
+
+	this.drag = function(){
+		// if nothing is selected or drag started off of selection, drag select - show drag select box, selection boxes appear
+		if(App.Game.currentPlanningLevel.currentSelection.length === 0 || !that.lmbStartOnTile){
+			var s = that.lmbDown;
+			var f = that.lmbUp;
+			App.Game.currentPlanningLevel.selectInstructions(s[0], s[1], s[2], f[0], f[1], f[2]);
+		}
+		// if drag started on selected instruction, move / copy - shadow graphics
+	}
+
+	this.single = function(){
+		if(!that.inserted){
+			var s = that.lmbDown;
+			var f = that.lmbUp;
+			App.Game.currentPlanningLevel.selectInstructions(s[0], s[1], s[2], f[0], f[1], f[2]);
 		}
 	}
 
@@ -134,11 +177,13 @@ App.PlanningGraphics = function(){
 			&& App.Game.currentPlanningLevel.currentSelection[0] !== null ){ that.selectionOverlay(gfx); } // TODO: move to static render?
 
 		// drag selection box
-		if(App.InputHandler.mmb === true){ that.drawSelectionBox(gfx); }
+		if(that.moving && App.Game.currentPlanningLevel.currentSelection.length === 0){ that.drawSelectionBox(gfx); }
+		else if(that.moving && that.lmbStartOnTile === false){ that.drawSelectionBox(gfx); }
+		// TODO can make into one if now that lmbStartOnTile changed?
 
 		// move / copy graphics
 		if(App.Game.currentPlanningLevel.currentSelection.length !== 0
-			&& that.moving){ that.moveCopy(gfx); }
+			&& that.moving && that.lmbStartOnTile){ that.moveCopy(gfx); }
 	}
 
 	this.moveCopy = function(gfx){
@@ -154,8 +199,8 @@ App.PlanningGraphics = function(){
 	this.drawSelectionBox = function(gfx){	
 		var curX = that.mousePos[0];
 		var curY = that.mousePos[1];
-		var downX = that.mmb[1];
-		var downY = that.mmb[2];
+		var downX = that.lmb[1];
+		var downY = that.lmb[2];
 		gfx.fillStyle = 'rgba(255,255,255,0.1)';
 		gfx.fillRect(curX, curY, (downX-curX), (downY-curY) );
 		gfx.strokeStyle = '#ffffff';
